e1dffcc32db0811a3ee6016dadbd082a47aca5fc
Daria <padfone66@gmail.com>
2016-03-21 14:19:44 +0100

    .

--- TrafficSimulation/TrafficSimulation/Properties/AssemblyInfo.cs
+++ TrafficSimulation/TrafficSimulation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TrafficSimulator")]
+[assembly: AssemblyTitle("TrafficSimulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TrafficSimulator")]
+[assembly: AssemblyProduct("TrafficSimulation")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2b7ad810-db37-48e9-ae7a-ebeb6d3e7db5")]
+[assembly: Guid("e591e7c5-3de9-4705-8ba5-5d3b04696147")]
 
 // Version information for an assembly consists of the following four values:
 //
--- TrafficSimulation/TrafficSimulation/Properties/Settings.Designer.cs
+++ TrafficSimulation/TrafficSimulation/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TrafficSimulator.Properties
+namespace TrafficSimulation.Properties
 {
 
 
