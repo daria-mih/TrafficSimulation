0e4a02605fb9c05c9ceb699e5db5d0cdd5632238
chiel zelders <kids-papa@hotmail.com>
2016-03-21 15:05:44 +0100

    this is the simulation class unimplemented

---
+++ TrafficSimulation/TrafficSimulation/Simulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulation
+{
+   static class Simulation
+    {
+       static public bool ShouldStop;
+       // public Grid FromGrid { get; set; }
+
+        //static private List<IMovables Moveables
+
+    }
+}
