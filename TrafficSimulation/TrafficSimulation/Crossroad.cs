4cf518136349d011c7504ef193eadf44a3bc0cfa
Daria <padfone66@gmail.com>
2016-03-22 16:41:23 +0100

    guys, we added sme stuff to your branch, check it out

---
+++ TrafficSimulation/TrafficSimulation/Crossroad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TrafficSimulation
+{
+    class Crossroad
+    {
+        //guys, we first put this method in direction but thought it would be better to use it in this class
+
+
+        List<Direction> routes;
+        //populates the list of the crossroad's available directions
+        public List<Point> AddNewDirection()
+        {
+            return direction;
+        }
+
+    }
+}
