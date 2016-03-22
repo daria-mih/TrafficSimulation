b7fead6989176244d0aef3a250638c2a2b4b21ff
jose luis peña <josepetew@hotmail.com>
2016-03-22 18:38:41 +0100

    added TrafficLight

---
+++ TrafficSimulation/TrafficSimulation/TrafficLight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TrafficSimulation
+{
+    class TrafficLight
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public Color State { get; set; }
+
+        public TrafficLight()
+        {
+
+        }
+
+        public void ChangeState(Color state)
+        {
+            State = state;
+        }
+    }
+}
