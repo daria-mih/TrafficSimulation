a2ab79051439008a55ecd95d0eb1178cd693daa3
jose luis peña <josepetew@hotmail.com>
2016-03-22 20:41:14 +0100

    fixed conflicts

--- TrafficSimulation/TrafficSimulation/Crossroad.cs
+++ TrafficSimulation/TrafficSimulation/Crossroad.cs
@@ -11,13 +11,43 @@ namespace TrafficSimulation
     {
         //guys, we first put this method in direction but thought it would be better to use it in this class
 
+        //We need to talk about this direction part.
+        List<Direction> routes { get; set; }
+       
+        public int NoOfCars { get; set; }
+        public int NoOfTrafficLights { get; set; }
+        public Crossroad North { get; set; }
+        public Crossroad South { get; set; }
+        public Crossroad East { get; set; }
+        public Crossroad West { get; set; }
 
-        List<Direction> routes;
-        //populates the list of the crossroad's available directions
-        public List<Point> AddNewDirection()
+        public Crossroad(int NoOfCars, int NoOfTrafficLights)
         {
-            return direction;
+            this.NoOfCars = 0;
+            this.NoOfTrafficLights = 0;
+            this.North = null;
+            this.South = null;
+            this.East = null;
+            this.West = null;
+            routes = new List<Direction>();
         }
 
+        //populates the list of the crossroad's available directions
+        public void AddDirections()
+        {
+            //something like this
+            //routes.Add(new Direction([Point(0,0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0)]));
+          
+        }
+        public void Connect()
+        { }
+
+        public Direction NextDirection()
+        {
+            //just an example ;)
+            //direction dir = new Direction( [Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0)])
+            //return dir;
+            return null; //for now
+        }
     }
 }
---
+++ TrafficSimulation/TrafficSimulation/TrafficLight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TrafficSimulation
+{
+    class TrafficLight
+    {
+        public int Id { get; set; }
+        public int Time { get; set; }
+        public Color State { get; set; }
+
+        public TrafficLight(int id, int time, Color state)
+        {
+            Id = id;
+            Time = time;
+            State = state;
+        }
+
+        public void ChangeState(Color state)
+        {
+            State = state;
+        }
+    }
+}
