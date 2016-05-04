d9b3d434f1781dee0f0f854cfc49097ca0c82235
armin <armin.ing34@gmail.com>
2016-05-04 11:33:58 +0200

    file added

--- TrafficSimulation/TrafficSimulation/Crossroad.cs
+++ TrafficSimulation/TrafficSimulation/Crossroad.cs
@@ -12,7 +12,7 @@ namespace TrafficSimulation
         //guys, we first put this method in direction but thought it would be better to use it in this class
 
         //We need to talk about this direction part.
-        List<Direction> routes { get; set; }
+       // List<Direction> routes { get; set; }
        
         public int NoOfCars { get; set; }
         public int NoOfTrafficLights { get; set; }
@@ -29,7 +29,7 @@ namespace TrafficSimulation
             this.South = null;
             this.East = null;
             this.West = null;
-            routes = new List<Direction>();
+          //  routes = new List<Direction>();
         }
 
         //populates the list of the crossroad's available directions
@@ -42,12 +42,12 @@ namespace TrafficSimulation
         public void Connect()
         { }
 
-        public Direction NextDirection()
-        {
-            //just an example ;)
-            //direction dir = new Direction( [Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0)])
-            //return dir;
-            return null; //for now
-        }
+        ////public Direction NextDirection()
+        ////{
+        ////    //just an example ;)
+        ////    //direction dir = new Direction( [Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0), Point(0, 0)])
+        ////    //return dir;
+        ////    return null; //for now
+        ////}
     }
 }
