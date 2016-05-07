90f87d9e72cea421562781dadac0b0701504d115
chiel zelders <kids-papa@hotmail.com>
2016-05-07 16:55:05 +0200

    working on the path finding

---
+++ TrafficSimulation/TrafficSimulation/Node.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulation
+{
+    class Node
+    {
+        //fields
+        public Crossroad crossNode;
+        public double fDistance;
+        public double sDistance;
+        public List<Node> neighbours;
+        //properties
+        public double Distance
+        {
+            get { return sDistance + fDistance; }
+        }
+
+        public Node parent;
+
+        //constructors
+            public Node(Crossroad nodeRoad)
+        {
+            crossNode = nodeRoad;
+        }
+        //methods
+        public void SetNeighbours(List<Node> allNodes)
+        {
+            foreach (Node node in allNodes)
+            {
+                if (node.crossNode == crossNode.North)
+                {
+                    neighbours.Add(node);
+                    continue;
+                }
+                if (node.crossNode == crossNode.East)
+                {
+                    neighbours.Add(node);
+                    continue;
+                }
+                if (node.crossNode == crossNode.South)
+                {
+                    neighbours.Add(node);
+                    continue;
+                }
+                if (node.crossNode == crossNode.West)
+                {
+                    neighbours.Add(node);
+                    continue;
+                }
+            }
+        }
+        public void SetDistance(Node startNode, Node finishNode)
+        {
+            sDistance = Math.Abs( Convert.ToDouble( (crossNode.Location.X + crossNode.Location.Y) - (parent.crossNode.Location.X + parent.crossNode.Location.Y)));
+            fDistance = Math.Abs(Convert.ToDouble((crossNode.Location.X + crossNode.Location.Y) - (finishNode.crossNode.Location.X + finishNode.crossNode.Location.Y)));
+        }
+
+    }
+}
--- TrafficSimulation/TrafficSimulation/Simulation.cs
+++ TrafficSimulation/TrafficSimulation/Simulation.cs
@@ -15,10 +15,77 @@ namespace TrafficSimulation
         //static private List<IMovables> Moveables;
         static List<Point> EndPoints;
 
+        private static List<Node> listOfNotes = new List<Node>();
+        
         //properties
         // public Grid FromGrid { get; set; }
 
         //methods
+        static private void GetNodes(Grid gridOfCrossroads)
+        {
+            foreach (Crossroad cRoad in gridOfCrossroads.Controls)
+            {
+                listOfNotes.Add(new Node(cRoad));
+            }
+            foreach (Node node in listOfNotes)
+            {
+                node.SetNeighbours(listOfNotes);
+            }
+            
+        }
+        static private List<Direction> GetShortestRoute(Node startPoint, Node endPoint)
+        {
+            //nodes to be processed
+              List<Node> openNodes = new List<Node>();
+            //nodes that are already processed
+              List<Node> closedNodes = new List<Node>();
+        //we need to start by adding the starting node as a node that needs to be proccessed
+        Node current = null;
+            openNodes.Add(startPoint);
+
+            while (openNodes.Count != 0)
+            {
+                foreach (Node node in openNodes)
+                {
+                    if (current == null)
+                    {
+                        current = node;
+                        continue;
+                    }
+                    if (current.Distance > node.Distance)
+                    {
+                        current = node;
+                    }
+                }
+                closedNodes.Add(current);
+                openNodes.Remove(current);
+                //you found the destination
+                if (current == endPoint)
+                {
+                    //time to create the Route
+                    //you are at the end so you will have to put the list from end to start
+                    List<Direction> templist = new List<Direction>();
+                    // templist.Insert(0, current.crossNode.the direction);
+                    while (current.parent != null)
+                    {
+                        // templist.Insert(0, current.crossNode.the direction);
+                        current = current.parent;
+                    }
+                    return templist;
+                }
+
+                //checking the neighbours
+                foreach (Node neighbour in current.neighbours)
+                {
+                    if (!openNodes.Contains(neighbour))
+                    {
+                        openNodes.Add(neighbour);
+                    }
+                }
+
+            }
+            return null;
+        }
         static private void CreateMovables()
         {
 
