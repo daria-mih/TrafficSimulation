da6fa20785cdfb212f5d74f5d70022abc477cecb
daria <d.mikhailovskaia@gmail.com>
2016-05-12 19:54:01 +0200

    started the move

--- TrafficSimulation/TrafficSimulation/Vehicle.cs
+++ TrafficSimulation/TrafficSimulation/Vehicle.cs
@@ -17,45 +17,27 @@ namespace TrafficSimulation
 
         public Graphics icon
         { get; set; }
-        public List<Direction> route
+        public List<Point> route
         { get; set; }
 
         public int size
         { get; set; }
 
-        public void Move(Direction direction) //should be a return method?
+        public Vehicle(List<Point> _route, Point startposition)
         {
-            //temporary list of directions
-            List<Direction> tempRoute = new List<Direction>();
-            Crossroad _crossroad = new Crossroad();
-            Crossroad prevCrossroad = new Crossroad();
-            List<Crossroad> tempCrossroads = new List<Crossroad>();
-            //Check which crossroad contains the currentposition
-            // -- Foreach crossroad in Grid, if crossroad contains currentposition, _crossroad = crossroad;
-            //Check if crossroad contains endpoint
-            //if(_crossroad.Contains(endPoint))
-            //{
+            route = _route;
+            size = 5;
+            currentPosition = startposition;
+        }
 
-            //tempRoute = ; 
-            //Select route that starts at currentPosition and ends at endPoint
-            //}
-            //else do a recursive check with all the neighbours of c
-            //else
-            //{
-            //    prevCrossroad = _crossroad;
-            //    if(prevCrossroad.East.Contains(endPoint) || prevCrossroad.North.Contains(endPoint) || prevCrossroad.South.Contains(endPoint)|| prevCrossroad.West.Contains(endPoint))
-            //    {
-            //_crossroad =  //Crossroad containing endpoint.
-            //tempRoute = ; //add route from prevCrossroad to _crossroad
-            //tempRoute = ; // add route from _crossroad to endpoint
-
-            //}
-            //else
-            //{
-            //do a check to see if (north, south, east or west of the neighbour of prevCrossroad contains point)
-            //else do another check to see if neighbours of the neighbours of prevCrossroad contains the endpoint)
-            //        }
-            //    }
+        public void Move()
+        {
+            //check if there is a car in front
+            //check if there is red light in front
+            //yet to be done
+            currentPosition = route[0];
+            route.Remove(route[0]); 
+            
         }
     }
 }
